f5fb07982ab6f85b9e945a25395bd51943d0c24e
Ed Lu <chaosed0@gmail.com>
2015-04-18 11:00:04 -0400

    World Map

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,39 +1,101 @@
 
-define(['crafty', 'jquery', './Util',
-        './Shape',
-        './Expires',
-    ], function(Crafty, $, u) {
+define(['jquery', './Util'], function($, u) {
 
     var self = this;
+
+    var labelProps = [
+        {
+            name: 'North America',
+            x: 0.2,
+            y: 0.3,
+        },
+        {
+            name: 'South America',
+            x: 0.3,
+            y: 0.6,
+        },
+        {
+            name: 'Africa',
+            x: 0.525,
+            y: 0.45,
+        },
+        {
+            name: 'Europe',
+            x: 0.55,
+            y: 0.225,
+        },
+        {
+            name: 'Asia',
+            x: 0.7,
+            y: 0.3,
+        },
+        {
+            name: 'Australia',
+            x: 0.85,
+            y: 0.675,
+        },
+    ];
     
-    var width = $(document).width();
-    var height = $(document).height();
-    var gameElem = document.getElementById('game');
+    var GameObj = function(selector) {
+        var obj = $(selector);
+        obj.position = {x: 0, y: 0};
+        obj.css('position', 'absolute');
+        obj.css('width', 'auto');
+        obj.css('height', 'auto');
 
-    Crafty.init(width, height, gameElem);  			  		
-    Crafty.viewport.clampToEntities = false;
+        obj.setPos = function(x, y) {
+            var pos;
+            if (y === undefined) {
+                pos = x;
+            } else {
+                pos = {x: x, y: y};
+            }
 
-    Crafty.scene("Load", function() {
+            u.assert(pos.x !== undefined && pos.y !== undefined);
 
-        console.log("LOAD");
-        
-        Crafty.background("#000");
-        Crafty.e("2D, DOM, Text").attr({ w:width, h: 20, x: 0, y: height/2 })
-                .text("Loading...")
-                .css({ "text-align": "center" });
+            this.css('left', pos.x);
+            this.css('top', pos.y);
+            this.position = pos;
+        }
 
-        var assets = { }
-        
-        //Preload assets first
-        Crafty.load(assets, function() {
-            Crafty.scene("Main");		
+        return obj;
+    }
+
+    var width = $(window).width();
+    var height = $(window).height();
+
+    var map = GameObj('#map');
+    var ratio = Math.min(width / map[0].clientWidth, height / map[0].clientHeight);
+    console.log(ratio);
+    map.width(map.width() * ratio);
+    map.height(map.height() * ratio);
+    map.setPos((width - map.width())/2.0, (height - map.height())/2.0);
+    map.css('z-index', -1000);
+
+    for (var i = 0; i < labelProps.length; i++) {
+        var props = labelProps[i];
+        var label = GameObj('<div/>').text(props.name);
+        label.css({
+            'font-size': '250%',
+            'user-select': 'none',
+            'cursor': 'default',
+            'text-align': 'center',
+            'display': 'flex',
+            'justify-content': 'center',
+            'align-content': 'center',
+            'flex-direction': 'column',
         });
-    });
 
-    Crafty.scene("Main", function () {
-        console.log("MAIN");
-        Crafty.background("#AAAAAA");
-    });
+        label.hover(function() {
+            $(this).css('color', '#0000FF');
+        }, function() {
+            $(this).css('color', '#000000');
+        });
 
-    Crafty.scene("Load");
+        $('body').append(label);
+        label.width(label.width()*1.5);
+        label.height(label.height()*3);
+        label.setPos(map.position.x + map.width() * props.x - label[0].clientWidth/2,
+                     map.position.y + map.height() * props.y - label[0].clientHeight/2);
+    }
 });
